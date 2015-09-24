41544b6145127eaf5df7bf154f954bfd5a6a25c1
Ivan <Ivan@130.203.105.16>
2015-09-24 08:56:07 -0400

    Created program to collect grades from a user and output an average

--- exercises/src/Grades.java
+++ exercises/src/Grades.java
@@ -1,3 +1,8 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
  * Repeatedly prompt the user to enter a grade.
  * 
@@ -19,10 +24,36 @@
  * 
  * You entered 2 grades. The average is 72.5
  */
-
 public class Grades {
-  public static void main(String[] args) {
-    
-  }
+
+    public static void main(String[] args) {
+        System.out.println("Please enter a series of grades ranging from 0 to 100. When finished, enter -1");
+        String input = null;
+        int grade = 0;
+        int gradeTotal = 0;
+        int gradeCounter = 0;
+        double gradeAverage = 0;
+
+        while (grade != -1) {
+            try {
+                System.out.print("Enter a grade: ");
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                input = bufferedReader.readLine();
+                grade = Integer.parseInt(input);
+                if (grade < 101 && grade > -1) {
+                    gradeCounter++;
+                    gradeTotal += grade;
+                } else if (grade != -1) {
+                    System.out.println("Error. Grade must be between 0 and 100; or -1 to finish.");
+                }
+            } catch (NumberFormatException ex) {
+                System.out.println("Not a number !");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        gradeAverage = (double) gradeTotal / gradeCounter;
+        System.out.println("You entered " + gradeCounter + " grades. The average is " + gradeAverage);
+    }
 
 }
